212d8b94f51b5f991d0c49bfe7f1bfa35ca9080d
Neelu <neelu@Neelus-MacBook-Air.local>
2025-09-16 05:27:54 -0700

    Add comments and fixed pom

--- department-service/src/main/java/com/example/department/client/UserClient.java
+++ department-service/src/main/java/com/example/department/client/UserClient.java
@@ -5,6 +5,20 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
+/*Feign Client is a declarative REST client provided by Spring Cloud.
+It lets you define an interface for calling another microservice,
+ without writing boilerplate RestTemplate or WebClient code.
+
+✅ Benefits
+
+->No need to manually construct HTTP requests
+
+->Works seamlessly with Eureka for service discovery
+
+->Integrates with Spring Cloud LoadBalancer for client-side load balancing
+
+Supports resilience patterns (Hystrix / Resilience4j)*/
 @FeignClient(name = "user-service")
         //, url = "http://localhost:8081")
 public interface UserClient {
